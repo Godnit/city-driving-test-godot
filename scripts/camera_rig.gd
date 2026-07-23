c51b3e2ca19bab151f94a281a338f99ba275f242
Godnit <gema.123.9000@gmail.com>
2026-07-23 14:31:37 +0300

    Fix camera script type inference and keep raised chase view

--- scripts/camera_rig.gd
+++ scripts/camera_rig.gd
@@ -12,18 +12,21 @@ const OFFSETS: Array[Vector3] = [
 const LOOK_DISTANCES: Array[float] = [9.5, 16.0, 21.0]
 const LOOK_HEIGHTS: Array[float] = [1.15, 1.30, 1.30]
 const MIN_HEIGHTS_ABOVE_TARGET: Array[float] = [2.20, 1.45, 1.15]
+const POSITIONAL_SPEEDS: Array[float] = [7.5, 11.5, 16.0]
+const ROTATION_SPEEDS: Array[float] = [8.5, 12.0, 16.0]
+const MODE_FOVS: Array[float] = [64.0, 68.0, 72.0]
 
 func setup(target_node: Node3D) -> void:
 	target = target_node
 	far = 420.0
 	near = 0.05
-	fov = 64.0
+	fov = MODE_FOVS[0]
 	current = true
 	_snap_to_target()
 
 func cycle_mode() -> void:
 	mode = (mode + 1) % OFFSETS.size()
-	fov = [64.0, 68.0, 72.0][mode]
+	fov = MODE_FOVS[mode]
 	set_cull_mask_value(2, mode != 2)
 	_snap_to_target()
 
@@ -35,8 +38,8 @@ func _process(delta: float) -> void:
 	desired_position.y = maxf(desired_position.y, target.global_position.y + MIN_HEIGHTS_ABOVE_TARGET[mode])
 	desired_position.y = maxf(desired_position.y, 1.65)
 	var look_target := target.global_position + Vector3.UP * LOOK_HEIGHTS[mode] - target_transform.basis.z * LOOK_DISTANCES[mode]
-	var positional_speed := [7.5, 11.5, 16.0][mode]
-	var rotation_speed := [8.5, 12.0, 16.0][mode]
+	var positional_speed: float = POSITIONAL_SPEEDS[mode]
+	var rotation_speed: float = ROTATION_SPEEDS[mode]
 	var position_alpha := 1.0 - exp(-positional_speed * delta)
 	var rotation_alpha := 1.0 - exp(-rotation_speed * delta)
 	var new_position := global_position.lerp(desired_position, position_alpha)
